77ad522ac1b38e2cddfd84526b8e89f7d5136efa
Vijay <vijaymurugan177@gmail.com>
2023-10-16 14:27:29 +0530

    FINISHED: Hero Section

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -4,6 +4,27 @@ const Hero = () => {
   return (
     <div className="w-full min-h-screen flex items-center flex-col section-hero">
       <Nav />
+      <div className="flex-1 flex flex-col justify-around items-center w-1/2">
+        <img src="/asserts/images/hero-logo.svg" alt="Haunted Plague" />
+        <div className="text-center">
+          <h1 className="text-3xl mb-2">SEASON 01</h1>
+          <p className="text-center text-lg">
+            The Haunted Plague NFT Collection is a spine-tingling digital art
+            series featuring 888 unique, eerie, and hauntingly beautiful NFTs.
+            Each piece is meticulously crafted to evoke a sense of mystery and
+            darkness, drawing inspiration from the supernatural and the macabre.
+            Explore this chilling collection to own a piece of digital art that
+            will send shivers down your spine.
+          </p>
+        </div>
+      </div>
+      <div className="">
+        <img
+          src="/asserts/images/group-of-hunters.svg"
+          alt="group-of-hunters"
+          className="h-52 m-2"
+        />
+      </div>
     </div>
   );
 };
--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,21 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Gloria_Hallelujah } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Gloria_Hallelujah({ subsets: ["latin"], weight: ["400"] });
 
 export const metadata = {
-  title: 'Haunted Plague NFT Collection',
-  description: 'The Haunted Plague NFT Collection is a spine-tingling digital art series featuring 888 unique',
-}
+  title: "Haunted Plague NFT Collection",
+  description:
+    "The Haunted Plague NFT Collection is a spine-tingling digital art series featuring 888 unique",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <link rel="icon" href="/asserts/images/fav-icon.svg" sizes="any" />
+        <link rel="icon" href="/asserts/images/fav-icon.svg" sizes="any" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
