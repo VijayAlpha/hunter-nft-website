e2b716b2b6db0e138a771c2902005a26e125d6dd
Vijay <vijaymurugan177@gmail.com>
2023-10-16 14:35:35 +0530

    ADDED: Base layout

---
+++ app/components/index.js
@@ -0,0 +1,9 @@
+import Hero from "./Hero";
+import Nav from "./Nav";
+import About from "./About";
+import Collections from "./Collections";
+import Team from "./Team";
+import FAQ from "./FAQ";
+import Footer from "./Footer";
+
+export { Hero, Nav , About , Collections , Team , FAQ , Footer};
--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
-import Hero from "./components/Hero";
+import { Hero, About, Collections, Team, FAQ, Footer } from "./components";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Hero />
+      <About />
+      <Collections />
+      <Team />
+      <FAQ />
+      <Footer />
     </main>
   );
 }
