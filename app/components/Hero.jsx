09d10743c4459b39222f9f5d36e210a23316b3bc
Vijay <vijaymurugan177@gmail.com>
2023-10-16 13:56:51 +0530

    ADDED: Nav list

---
+++ app/components/Hero.jsx
@@ -0,0 +1,11 @@
+import Nav from "./Nav";
+
+const Hero = () => {
+  return (
+    <div className="w-full min-h-screen flex items-center flex-col section-hero">
+      <Nav />
+    </div>
+  );
+};
+
+export default Hero;
---
+++ app/components/Nav.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul className="flex gap-3 m-4">
+        <li>
+          <a href="#home">
+            <img src="/asserts/images/nav-home.svg" alt="" className="w-32" />
+          </a>
+        </li>
+        <li>
+          <a href="#about">
+            <img src="/asserts/images/nav-about.svg" alt="" className="w-32" />
+          </a>
+        </li>
+        <li>
+          <a href="#collections">
+            <img src="/asserts/images/nav-collections.svg" alt="" className="w-32" />
+          </a>
+        </li>
+        <li>
+          <a href="#team">
+            <img src="/asserts/images/nav-team.svg" alt="" className="w-32" />
+          </a>
+        </li>
+        <li>
+          <a href="#faq">
+            <img src="/asserts/images/nav-faq.svg" alt="" className="w-32" />
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
