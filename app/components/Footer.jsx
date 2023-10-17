289bf255131a6e8bada636043380a85e2f307a0c
Vijay <vijaymurugan177@gmail.com>
2023-10-17 15:53:49 +0530

    FINISHED:  Footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,8 +1,35 @@
-
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="section-footer flex flex-col items-center w-full relative min-h-screen">
+      <div className="flex-1 flex flex-col justify-around items-center absolute bottom-0">
+        <img
+          src="/asserts/images/hero-logo.svg"
+          alt="Haunted Plague"
+          className="w-1/2"
+        />
+        <div className="text-center [&>*]:m-10 ">
+          <p className="text-center text-lg max-w-md">
+            Lorem ipsum dolor sit amet consectetur. Sed cras integer diam ac
+            morbi adipiscing sodales
+          </p>
+          <div className="flex gap-4 justify-center">
+            <img
+              src="/asserts/images/icon-x.svg"
+              alt=""
+              className="cursor-pointer"
+            />
+            <img
+              src="/asserts/images/icon-discord.svg"
+              className="cursor-pointer"
+            />
+          </div>
+          <p className="text-center">
+            © 2023 Haunted Plague, All Rights Reserved
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
