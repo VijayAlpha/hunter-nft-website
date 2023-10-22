64daa86545b40c9c81e369347a4baac78b5403a5
Vijay <vijaymurugan177@gmail.com>
2023-10-22 11:20:50 +0530

    ADDED: Roadmap section

--- app/components/Roadmap.jsx
+++ app/components/Roadmap.jsx
@@ -1,9 +1,11 @@
 const Roadmap = () => {
   return (
-    <section className="flex flex-col items-center">
-      <h2 className="text-4xl">Road Map</h2>
-      <div className="">
-        <img src="/asserts/images/roadmap-lock.svg" />
+    <section className="flex flex-col items-center bg-[#0E080A]">
+      <img src="/asserts/images/name-roadmap.svg" className="mb-10" />
+
+      <div className="relative flex items-center justify-center">
+        <img src="/asserts/images/name-coming-soon.svg" className="absolute" />
+        <img src="/asserts/images/roadmap.png" className="w-screen " />
       </div>
     </section>
   );
--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
       <Hero />
       <About />
       <Collections />
-      {/* <Roadmap /> */}
+      <Roadmap />
       <Team />
       <FAQ />
       <Footer />
